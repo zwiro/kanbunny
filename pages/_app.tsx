df8e01335f558a2498695b3755dd69b9e0e35421
zwiro <josiu100@gmail.com>
2023-04-17 16:48:46 +0200

    Added next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,18 @@ import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import type { AppType } from "next/app"
 import { trpc } from "../utils/trpc"
+import { SessionProvider } from "next-auth/react"
 
-const App: AppType = ({ Component, pageProps }: AppProps) => {
+const App: AppType = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
 
