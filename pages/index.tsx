d2be169d67da791d219a290b8e140d0726ec902a
zwiro <josiu100@gmail.com>
2023-04-17 18:15:45 +0200

    Added github auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return <div>index</div>
+  const { data: session, status } = useSession()
+  console.log(session, status)
+  if (status === "loading") {
+    return <p>Hang on there...</p>
+  }
+
+  if (status === "authenticated") {
+    return (
+      <>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p>Not signed in.</p>
+      <button onClick={() => signIn("github")}>Sign in</button>
+    </>
+  )
 }
