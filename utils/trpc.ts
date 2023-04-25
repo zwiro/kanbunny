d9e6f2838cf4b8704ed36897c6e085954dfb0d47
zwiro <josiu100@gmail.com>
2023-04-25 20:21:46 +0200

    trpc util fix

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,6 +1,8 @@
 import { httpBatchLink } from "@trpc/client"
 import { createTRPCNext } from "@trpc/next"
 import type { AppRouter } from "../server/routers/_app"
+import superjson from "superjson"
+
 function getBaseUrl() {
   if (typeof window !== "undefined")
     // browser should use relative path
@@ -15,8 +17,9 @@ function getBaseUrl() {
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
 export const trpc = createTRPCNext<AppRouter>({
-  config({ ctx }) {
+  config() {
     return {
+      transformer: superjson,
       links: [
         httpBatchLink({
           /**
