332a8adde6e30a0dfc03fac53729986bd9e28e03
zwiro <josiu100@gmail.com>
2023-04-25 15:56:17 +0200

    Added prisma models

---
+++ server/routers/project.ts
@@ -0,0 +1,8 @@
+// import { z } from "zod"
+// import { protectedProcedure, router } from "../trpc"
+
+// export const projectRouter = router({
+//     create: protectedProcedure.input(z.object({
+//         name: z.string(),
+//     }))
+//     })
