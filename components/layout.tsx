6e89feff3705125debf506591c8f8a98f382346b
zwiro <josiu100@gmail.com>
2023-06-26 17:48:38 +0200

    Added head title and favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,8 @@
 import { LayoutProvider } from "@/context/LayoutContext"
 import { Jura } from "next/font/google"
 import Navbar from "./Navbar"
-import ConfirmPopup from "./ConfirmPopup"
+import Head from "next/head"
+import { useSession } from "next-auth/react"
 
 const jura = Jura({ subsets: ["latin"], weight: ["400", "700"] })
 
@@ -10,8 +11,39 @@ interface LayoutProps {
 }
 
 function Layout({ children }: LayoutProps) {
+  const { data: session, status } = useSession()
+
   return (
     <LayoutProvider>
+      <Head>
+        <title>kanbunny {session ? `| ${session.user.name}` : ""}</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="images/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="images/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <div
         className={`${jura.className} min-h-screen bg-zinc-700 text-slate-100`}
       >
