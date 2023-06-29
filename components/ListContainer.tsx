8969d1b894ecdd07f413141a41edfc0f6254ccbf
zwiro <josiu100@gmail.com>
2023-06-29 15:32:03 +0200

    CSS tweaks

--- components/ListContainer.tsx
+++ components/ListContainer.tsx
@@ -4,7 +4,7 @@ interface ListContainerProps {
 
 function ListContainer({ children }: ListContainerProps) {
   return (
-    <section className="mt-4 flex h-min min-w-[18rem] flex-col gap-4 border border-t-4 border-neutral-700 border-t-blue-500 bg-zinc-800 p-4 pb-7">
+    <section className="ml-4 mt-4 flex h-min min-w-[18rem] flex-col gap-4 border border-t-4 border-neutral-700 border-t-blue-500 bg-zinc-800 p-4 pb-7 lg:ml-8 xl:ml-16">
       <div className="flex items-center gap-2">
         <div className="h-4 w-4 bg-blue-500" />
         {children}
